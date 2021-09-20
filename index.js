affd9991e113b11389a13b9d4a9298c105d3110f
@Joe_Sealy <joe.sealy@hotmail.co.uk>
2021-09-20 01:14:51 +0100

    starting again

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const Discord = require('discord.js');
-const config = require('./Data/config.json');
+const Discord = require("discord.js");
+const config = require("./Data/config.json");
 const intents = new Discord.Intents(32767);
-const client = new Discord.Client({ intents });
+const client = new Discord.Client({intents });
 
-client.queue =  new Map();
 
-client.login(config.token);
+client.on("Ready",() =>{
+    console.log("All good my g");
+})
+
+
+client.on("messageCreate", (message) =>{
+
+
+
+
+})
+
+
+
+
+client.token(config.token);
