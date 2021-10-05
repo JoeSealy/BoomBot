57066c74275cb3a603dceb71b4d4baae0464fe88
@Joe_Sealy <joe.sealy@hotmail.co.uk>
2021-10-05 01:26:34 +0100

    extra skip message and !p works i think

--- BoomBot/index.js
+++ BoomBot/index.js
@@ -1,11 +1,10 @@
 const Discord = require("discord.js");
 const config = require("./Data/config.json");
-const {DisTube, DisTubeError} = require('distube');
+const {DisTube, DisTubeError, Playlist} = require('distube');
 const intents = new Discord.Intents(32767);
 const client = new Discord.Client({intents });
 
 const distube = new DisTube(client, {searchSongs: 1, emitNewSongOnly: true});
-const effectsArray = new Array("3d", "bassboost", "echo", "karaoke", "nightcore", "vaporwave");
 client.on("ready",() =>{
     console.log("All good my g");
 })
@@ -33,21 +32,21 @@ client.on("messageCreate", async (message) =>{
     }
     if(command === ("help"))
     {
-        message.channel.send("$play, $stop, $skip, $queue, options for songs === $3d, $bassboost, $echo, $karaoke, $nightcore, $vaporwave")
+        message.channel.send("Start the command !   then..  play, stop, skip, repeat, seek{time(seconds)}, queue. Effects == 3d, bassboost, echo, karaoke, nightcore, vaporwave");
     }
 
 
     //MUSIC TIME 
-    if(command === (("play"||"p")))
+    if(command === (("play")||("p")))
     {
-        distube.play(message, args.join(" ")); 
-        message.channel.send(
-            "i be playing..."
-        )
-        message.channel.send({
-            content: message.content.slice(5)
+        distube.play(message, args.join(" "));
+        message.channel.send("I be playin it");
+    }
 
-        })
+    if (command == "repeat") {
+        let mode = distube.setRepeatMode(message, parseInt(args[0]));
+        mode = mode ? mode == 2 ? "Repeat queue" : "Repeat song" : "Off";
+        message.channel.send("Set repeat mode to `" + mode + "`");
     }
 
     if(command  === ("stop"))
@@ -58,11 +57,17 @@ client.on("messageCreate", async (message) =>{
 
     }
 
-    if(command === ("skip"))
+    if(command === (("skip") && (distube.off)))
+    {
+        message.channel.send("Yo i cant be skipping nothing nigga!!!")
+    }
+    else if(command === ("skip"))
     {
         distube.skip(message);
         message.channel.send("I skipped this mofo fo yo");
     }
+
+
 
     if(command ===("seek"))
     {
@@ -83,7 +88,6 @@ client.on("messageCreate", async (message) =>{
         const filter = distube.setFilter(message, command);
         message.channel.send("Current queue filter: " + (filter.join(", ") || "Off"));
     }
-
 })
 
 
